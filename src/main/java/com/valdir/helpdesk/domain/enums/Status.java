2800a9190b3e810fa2921d2377a02287194371ab
sidneyperota <sidneyperota@gmail.com>
2023-06-11 14:41:30 -0300

    criacao dominios

---
+++ src/main/java/com/valdir/helpdesk/domain/enums/Status.java
@@ -0,0 +1,48 @@
+package com.valdir.helpdesk.domain.enums;
+
+public enum Status {
+   ABERTO(0,"ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2,"ENCERRADO"); 
+	
+   private Integer codigo; 
+   private String descricao;
+public Integer getCodigo() {
+	return codigo;
+}
+public void setCodigo(Integer codigo) {
+	this.codigo = codigo;
+}
+public String getDescricao() {
+	return descricao;
+}
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+private Status(Integer codigo, String descricao) {
+	this.codigo = codigo;
+	this.descricao = descricao;
+}
+   
+public static Status toEnum( Integer cod ) { 
+	if ( cod == null ) { 
+		return null;
+	}
+	
+	for ( Status x : Status.values() ) { 
+		if ( cod.equals(x.codigo) ) {
+			return x;
+
+		}
+	
+	}
+	
+	throw new IllegalArgumentException("Status inválido");
+	
+	
+	
+	
+}
+
+   
+   
+	
+}
